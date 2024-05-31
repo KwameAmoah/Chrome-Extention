dc045179abf3db2395581a6c09ff255918b366a7
Kwame Amoah <105723968+KwameAmoah@users.noreply.github.com>
2024-05-31 22:28:44 +0000

    My first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    renderLeads()
+})
+
+// Render the leads in the unordered list using ulEl.textContent
+function renderLeads () {
+    let listItems = " "
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+
+    //another way to add elements to html
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+}
+
+ulEl.innerHTML = listItems
+}
